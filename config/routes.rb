423ffddf6db6f28dae3922832af93dc59363ce33
Samuel Zemmour <sazemmour@sergent-major.com>
2023-02-22 15:45:16 +0100

    update restaurant review

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:new, :show, :index, :create, :destroy] do
     resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
